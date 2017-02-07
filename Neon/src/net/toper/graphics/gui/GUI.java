0acc050612856b7bb5c455bf1e4c332aa9c3002f
Jameson Toper <toperdev@gmail.com>
2017-02-06 23:17:17 -0500

    GUI Main Menu Update
    Got fonts working well and allowed each element to have its own font,
    which means special effects can be applied to each elements text
    
    Got the main menu buttons working, start turns red when the mouse is
    hovered over
    
    Improved performance on rendering on the main menu, though may need an
    FPS cap since it runs so quickly.
    
    Experimenting with smooth deltas now

--- Neon/src/net/toper/graphics/gui/GUI.java
+++ Neon/src/net/toper/graphics/gui/GUI.java
@@ -3,13 +3,13 @@ package net.toper.graphics.gui;
 import java.util.ArrayList;
 import java.util.List;
 
-import net.toper.graphics.Font;
+import net.toper.graphics.FontManager;
 
 public class GUI {
 
 	private List<GUIElement> elements = new ArrayList<GUIElement>();
 
-	private static Font font = new Font();
+	private static FontManager font = new FontManager();
 	private static GameState state;
 
 	private static GUIMenuMain main = new GUIMenuMain();
@@ -48,7 +48,7 @@ public class GUI {
 
 	}
 
-	public static Font getFont() {
+	public static FontManager getFont() {
 		return font;
 	}
 
--- Neon/src/net/toper/graphics/gui/GUIMenuMain.java
+++ Neon/src/net/toper/graphics/gui/GUIMenuMain.java
@@ -11,18 +11,17 @@ public class GUIMenuMain extends GUI {
 	public GUIMenuMain() {
 		setFPSCap(60);
 		addElement(new GUIElementText(460, 100, "SUCC"));
-		startButtonID = addElement(new GUIElementText(460, 200, "Start"));
-		clickStartID = addElement(new GUIMouseInteract(this, startButtonID));
+		startButtonID = addElement(new GUIElementText(460, 200, "Start",30));
+		clickStartID = addElement(new GUIElementClickable(this, startButtonID));
 	}
 
 	public void updateLogic() {
-		if (((GUIMouseInteract) getElement(clickStartID)).clicked()) {
+		if (((GUIElementClickable) getElement(clickStartID)).clicked()) {
 			GUI.setState(GameState.PLAYING);
 		}
-		if (((GUIMouseInteract) getElement(clickStartID)).hover()) {
-			getElement(clickStartID).getFont().setColor(new Color(1, 0, 0));
-		} else {
-			getElement(clickStartID).getFont().setColor(Color.white);
+		getElement(startButtonID).setColor(Color.white);
+		if (((GUIElementClickable) getElement(clickStartID)).hover()) {
+			getElement(startButtonID).setColor(Color.red);
 		}
 	}
 
